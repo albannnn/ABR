69a93efa94d0585edd59a3b54a72487d97964da0
albannnn <80924616+albannnn@users.noreply.github.com>
2023-02-19 15:03:51 +0100

    Update ABR_numgroupe.py

--- projetArbreBinaireDeRecherche/ABR_numgroupe.py
+++ projetArbreBinaireDeRecherche/ABR_numgroupe.py
@@ -1,5 +1,5 @@
 ## Projet : Arbre Binaire de Recherche
-from BinTree.py import BinTree
+from BinTree import BinTree
 ## PROGRAMME PRINCIPAL
 arbreBinaire = BinTree(30, BinTree(25, BinTree(18, BinTree(9)), BinTree(29, BinTree(26, None, BinTree(28)))), BinTree(41, None, BinTree(52, BinTree(48), BinTree(60))) )
 
