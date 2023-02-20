f97a578ecb129e15e10aeebaade916127214c728
albannnn <80924616+albannnn@users.noreply.github.com>
2023-02-20 13:24:08 +0100

    Delete ABR_numgroupe.py

--- projetArbreBinaireDeRecherche/ABR_numgroupe.py
+++
@@ -1,7 +0,0 @@
-## Projet : Arbre Binaire de Recherche
-from BinTree import BinTree
-## PROGRAMME PRINCIPAL
-arbreBinaire = BinTree(30, BinTree(25, BinTree(18, BinTree(9)), BinTree(29, BinTree(26, None, BinTree(28)))), BinTree(41, None, BinTree(52, BinTree(48), BinTree(60))) )
-
-## TESTS
-print(arbreBinaire)
