d6f2566d9154e65b0ca79d04af59ad0b10e5d5fe
albannnn <80924616+albannnn@users.noreply.github.com>
2023-03-05 22:10:05 +0100

    Update classABR.py

--- projetArbreBinaireDeRecherche/classABR.py
+++ projetArbreBinaireDeRecherche/classABR.py
@@ -5,11 +5,11 @@ class ABR(BinTree):
         """ ABR test """
         if self.estVide() or self.estFeuille():
             return True
-        if not self.gauche().estVide() and self.gauche().maximum() and self.racine():
-                return False
+        if not self.gauche().estVide() and self.gauche().maximum() > self.racine():
+            return False
         if not self.droit().estVide() and self.droit().minimum() <= self.racine():
-                return False
-        return self.gauche().ABR() and self.droit().ABR()
+            return False
+        return self.gauche().__estABR__() and self.droit().__estABR__()
      
     def __init__(self, root = None, left = None, right = None):
         BinTree.__init__(self, root, left, right)
@@ -17,7 +17,7 @@ class ABR(BinTree):
         if not self.__estABR__():
             raise ValueError("Les valeurs de l'arbre ne permettent pas la création d'un arbre binaire de recherche")
          
-    ## Impression : surcharge de la méthode str car on change les carctères entre les noeuds par > et <= 
+    ## Impression : surcharge de la méthode str car on change les carctères entre les racines par > et <= 
     def __str__(self,n=0,car=""):
         """
         Fournit une vue lisible de l'arbre.
@@ -43,10 +43,11 @@ class ABR(BinTree):
         #assertion
         assert type(n) is int , 'n doit être un entier !'
         #programme
-        if self.estVide():
-            return False #Si ojn tombe sur un arbre vide -> les arbres se sont echainés et à aucun moment la valeur d'une racine n'à été égale à la valeur cherchée, cette dernière n'est donc pas dans l'arbre
+        
         if self.racine() == n: #Si la racine est le nombre cherché -> renvoie True, la valeur est bien dans l'arbre
             return True
+        if self.estVide() or self.estFeuille():
+            return False #Si on tombe sur un arbre vide -> les arbres se sont echainés et à aucun moment la valeur d'une racine n'à été égale à la valeur cherchée, cette dernière n'est donc pas dans l'arbre
          
         else:  #Dans les autres cas il faut parcourir récursivement l'arbre -> 2 cas possibles dans un ABR
             if n <= self.racine():  #1er cas, la valeur de n est inférieure ou égale à la racine -> elle se trouve donc dans le SAG
@@ -54,18 +55,21 @@ class ABR(BinTree):
             else: #dans tout les autres la valeur de n est strictement supérieure à celle de racine -> elle se trouve donc dans le SAD
                 return self.droit().isInABR(n) #Résolution à partir de SAD
  
-    def insereCle(self, n:int) :
+    def insereCle(self, n:int) -> None:
         """
         Renvoie un ABR avec la clé insérée au bon endroit
         """
- 
-        if self.estVide(): #Si l'arbre est vide, on peut ajouter une clé sans contraintes
-            return ABR(n)
-        if n <= self.racine(): #D'après la définition de l'ABR, si la valeur est <= à la racine, alors elle est dans la partie gauche de l'arbre
-            return ABR(self.racine(),self.gauche().insereCle(n), self.droit())
-        else: #dans tous les autres cas, elle est dans la partie droite de l'arbre
-            return ABR(self.racine(), self.gauche(), self.droit().insereCle(n))
-         
+        if self.racine() >= n:
+            if self.gauche().estVide():
+                self.setGauche(ABR(n))
+            else:
+                self.gauche().insereCle(n)
+        else:
+            if self.droit().estVide():
+                self.setDroit(ABR(n))
+            else:
+                self.droit().insereCle(n)
+        
     def sommeCle(self) -> int:
         """
         Renvoie la somme des clés de l'ABR donné en args
@@ -75,3 +79,14 @@ class ABR(BinTree):
         else:
             return self.racine() + self.gauche().sommeCle() + self.droit().sommeCle()
  
+
+ 
+arbre = ABR(3, 
+            ABR(1), 
+            ABR(5))
+
+arbre.insereCle(8) #8 est mtn dans l'abr
+
+print(arbre)
+print(arbre.isInABR(8)) #return True
+print(arbre)
