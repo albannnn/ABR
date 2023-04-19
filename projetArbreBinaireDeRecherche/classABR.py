bbaf495e4556f5d43a4080839871decd622bb026
albannnn <80924616+albannnn@users.noreply.github.com>
2023-04-19 17:10:59 +0200

    Add files via upload

--- projetArbreBinaireDeRecherche/classABR.py
+++ projetArbreBinaireDeRecherche/classABR.py
@@ -18,20 +18,20 @@ class ABR(BinTree):
             raise ValueError("Les valeurs de l'arbre ne permettent pas la création d'un arbre binaire de recherche")
          
     ## Impression : surcharge de la méthode str car on change les carctères entre les racines par > et <= 
-    def __str__(self,n=0,car=""):
+    def __str__(self, n=0, car=""):
         """
         Fournit une vue lisible de l'arbre.
         """
-        if not(self.estVide()):
-            self.droit().__str__(n+4,'<')
-            print(n*" "+car,end=" ")
+        if not (self.estVide()):
+            self.droit().__str__(n+4, '<')
+            print(n*" "+car, end=" ")
             print(self.racine())
-            self.gauche().__str__(n+4,'>=')
-        elif n==0:
-            #Imprime "arbre_vide" si l'arbre est vide (lors de l'appel initial)
+            self.gauche().__str__(n+4, '>=')
+        elif n == 0:
+            # Imprime "arbre_vide" si l'arbre est vide (lors de l'appel initial)
             print("Arbre_vide")
- 
-        #La fonction doit renvoyer une chaîne, mais l'impression est déjà gerée plus haut
+
+        # La fonction doit renvoyer une chaîne, mais l'impression est déjà gerée plus haut
         return ""
  
  
@@ -54,21 +54,21 @@ class ABR(BinTree):
                 return self.gauche().isInABR(n) # Résolution à partir du SAG
             else: #dans tout les autres la valeur de n est strictement supérieure à celle de racine -> elle se trouve donc dans le SAD
                 return self.droit().isInABR(n) #Résolution à partir de SAD
- 
+
     def insereCle(self, n:int) -> None:
         """
-        Renvoie un ABR avec la clé insérée au bon endroit
+        Ne renvoie rien . L'abr qui a utilisé cette méthode a quand à lui une clé insérée automatiquement au bon endroit'
         """
-        if self.racine() >= n:#1ere chose à faire, savoir si la racine est supérieure ou inférieure à la valeure recherchée
-            if self.gauche().estVide():# Si l'arbre dans lequel doit se trouver la valeur à insérer est vide on insère la valeur
-                self.setGauche(ABR(n))
+        if self.racine() >= n:
+            if self.gauche().estVide():
+                self.setGauche(type(self)(n))
             else:
-                self.gauche().insereCle(n)#sinon appel récursif de la fonction à partir de l'arbre dans lequel elle doit se trouver
+                self.gauche().insereCle(n)
         else:
             if self.droit().estVide():
-                self.setDroit(ABR(n))
+                self.setDroit(type(self)(n))
             else:
-                self.droit().insereCle(n)#pareil avec l'arbre droit 
+                self.droit().insereCle(n)
         
     def sommeCle(self) -> int:
         """
@@ -78,9 +78,11 @@ class ABR(BinTree):
             return 0
         else:
             return self.racine() + self.gauche().sommeCle() + self.droit().sommeCle()
-    
-    def supprimeCle(self, n:int) -> None:
-        """ Méthode permettant de supprimer un noeud de l'abr """
+        
+    def supprimeCle(self, n:int) -> None: 
+        """
+        Méthode permettant de supprimer un noeud de l'abr 
+        """
         assert type(n) is int, "La valeur à supprimer doit être un entier"
         assert self.isInABR(n), "La valeur à supprimer doit être dans l'arbre"
         # 3 cas possibles, 1° La valeur à supprimer est une feuille,2° La valeur à supprimer n'a qu'un enfant,3° La valuer à supprimer a 2 enfants
@@ -90,7 +92,7 @@ class ABR(BinTree):
             self.droit().supprimeCle(n)
         else: # Quand on a trouvé la valeur recherché : Alors on en vient aux 3 cas possibles expliqués plus tôt
             if self.estFeuille():
-                self.setRacine(ABR()) # 1er cas : on supprime la valeur en la remplaçant par un ABR vide
+                self.setRacine(None) # 1er cas : on supprime la valeur en la remplaçant par un ABR vide
             elif self.droit().estVide() :
                 self.setRacine(self.gauche()) #2eme cas : on remplace la racine par le sag si pas de sad
             elif self.gauche().estVide():
@@ -98,14 +100,18 @@ class ABR(BinTree):
             else:
                #Dernier cas, il faut trouver le successeur du noeud : c'est à dire le plus grand de son sag ou le minimum de son sad. Nous utiliserons le 1er cas avec le + grand du sag
                   successeur = self.gauche().maximum()
-                  self.setRacine(successeur)
                   self.supprimeCle(successeur)
-
- 
-arbre = ABR(3, ABR(1), ABR(5))
-
-arbre.insereCle(8) #8 est mtn dans l'abr
-
+                  self.setRacine(successeur)
+                  
+            
+arbre = ABR(3, 
+            ABR(1), 
+            ABR(5, ABR(4), ABR(9)))
 print(arbre)
-print(arbre.isInABR(8)) #return True
+arbre.insereCle(8) #8 est mtn dans l'abr
+arbre.insereCle(2)
+arbre.insereCle(12)
+print(arbre)
+arbre.supprimeCle(5)
+
 print(arbre)
